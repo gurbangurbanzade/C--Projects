b2720de5a673f1fae425294895c0c7815ce25163
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2022-10-29 17:36:00 +0400

    variables example

---
+++ C# Exercises/variables/variables/Program.cs
@@ -0,0 +1,20 @@
+﻿// tam eded
+int number = 5;
+Console.WriteLine(number);
+
+// kesr eded
+float number2 = 12.5f;
+double number3 = 12.6;
+decimal number4 = 12.4m;
+Console.WriteLine(number2);
+Console.WriteLine(number3);
+Console.WriteLine(number4);
+
+// string
+string name = "gurban gurbanzada";
+char simvol = 'a';
+Console.WriteLine(simvol);  
+Console.WriteLine(name);
+
+// boolean
+bool result = true;
