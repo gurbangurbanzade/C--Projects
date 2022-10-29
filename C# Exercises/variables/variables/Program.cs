5eb914161a0106a0f213d459e31ae6a7c4ae0029
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2022-10-29 21:37:40 +0400

    string example

--- C# Exercises/variables/variables/Program.cs
+++ C# Exercises/variables/variables/Program.cs
@@ -1,20 +1,42 @@
-﻿// tam eded
-int number = 5;
-Console.WriteLine(number);
+﻿//// tam eded
+//int number = 5;
+//Console.WriteLine(number);
 
-// kesr eded
-float number2 = 12.5f;
-double number3 = 12.6;
-decimal number4 = 12.4m;
-Console.WriteLine(number2);
-Console.WriteLine(number3);
-Console.WriteLine(number4);
+//// kesr eded
+//float number2 = 12.5f;
+//double number3 = 12.6;
+//decimal number4 = 12.4m;
+//Console.WriteLine(number2);
+//Console.WriteLine(number3);
+//Console.WriteLine(number4);
+
+//// string
+//string name = "gurban gurbanzada";
+//char simvol = 'a';
+//Console.WriteLine(simvol);  
+//Console.WriteLine(name);
+
+//// boolean
+//bool result = true;
+
+
+//// toplama emeliyyati
+//Console.Write("number1-");
+//int number5 = int.Parse(Console.ReadLine());
+//Console.Write("number2-");
+//int number6 = int.Parse(Console.ReadLine());
+//int sum = number5 + number6;
+//Console.WriteLine($"cavab- {sum}");
 
 // string
-string name = "gurban gurbanzada";
-char simvol = 'a';
-Console.WriteLine(simvol);  
-Console.WriteLine(name);
 
-// boolean
-bool result = true;
+string name = "gurban";
+string surname = "gurbanzada";
+int age = 29;
+
+string str = $"my name is {name} {surname} , {age} years ";
+string str1= string.Format("may name is {0} {1} , {2} years", name, surname, age);
+Console.WriteLine(str);
+Console.WriteLine(str1);
+
+
